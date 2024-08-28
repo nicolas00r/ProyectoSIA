32ae5501114b29bd7168136827dd8b0b55d38edc
JoaquinGarridoRiquelme <joaquin.garrido.r@mail.pucv.cl>
2024-08-27 21:52:27 -0400

    Actualizacion de clase mascota
    Se incluyo id para cada mascota y nombre para el dueño de la mascota.

--- Clases/Mascota.java
+++ Clases/Mascota.java
@@ -1,29 +1,44 @@
+package Clases;
+
 public class Mascota{
     //VARIABLES
-    private String nombre;
+    private String nombreMascota;
+    private String nombreDueño;
     private String especie;
     private int edad;
+    private int id;
+
+    private static int contadorId = 0;
 
     //CONSTRUCTOR
-    public Mascota(String nombre, String especie, int edad){
-        setNombre(nombre);
+    public Mascota(String nombreMascota, String especie, int edad){
+        this.id = ++contadorId;
+        setNombreMascota(nombreMascota);
+        setNombreDueño(nombreDueño);
         setEspecie(especie);
         setEdad(edad);
     }
 
     //METODOS
-    public void setNombre(String nombre){this.nombre = nombre;}
-    public String getNombre(){return nombre;}
+    public void setNombreMascota(String nombreMascota){this.nombreMascota = nombreMascota;}
+    public String getNombreMascota(){return nombreMascota;}
+
+    public void setNombreDueño(String nombreDueño){this.nombreDueño = nombreDueño;}
+    public String getNombreDueño(){return nombreDueño;}
 
     public void setEspecie(String especie){this.especie = especie;}
     public String getEspecie(){return especie;}
+
+    public int getId(){return id;}
 
     public void setEdad(int edad){this.edad = edad;}
     public int getEdad(){return edad;}
 
     public void mostrarDatosMascota(){
         System.out.println("Datos de la mascota");
-        System.out.println("Nombre: " + nombre);
+        System.out.println("Nombre de la mascota: " + nombreMascota);
+        System.out.println("Nombre del dueño: " + nombreDueño);
+        System.out.println("Id de la mascota: " + id);
         System.out.println("Especie: " + especie);
         System.out.println("Edad: " + edad);
     }
