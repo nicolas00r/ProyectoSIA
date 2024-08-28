fa46370a5f112ce7ca23f2c313826fb3dc6dc40c
F4CH <161516529+F4CH@users.noreply.github.com>
2024-08-27 20:39:29 -0400

    Creacion de las clases cita y servicio
    Creacion de las clases con sus respectivos constructores y metodos

---
+++ Clases/Cita.java
@@ -0,0 +1,48 @@
+package Clases;
+
+//Variables
+public class Cita{
+    private Cliente cliente;
+    private Mascota mascota;
+    private Servicio servicio;
+    private String fechaHora;
+    // Constructor 
+    public Cita(Cliente cliente, Mascota mascota, Servicio servicio, String fechaHora){
+        this.cliente = cliente;
+        this.mascota = mascota;
+        this.servicio = servicio;
+        this.fechaHora = fechaHora;
+    }
+    // Metodos
+    public Cliente getCliente(){
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+
+    public Mascota getMascota(){
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota){
+        this.mascota = mascota;
+    }
+
+    public Servicio getServicio(){
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio){
+        this.servicio = servicio;
+    }
+
+    public String getFechaHora(){
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora){
+        this.fechaHora = fechaHora;
+    }
+}
